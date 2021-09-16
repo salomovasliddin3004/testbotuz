b803cf1a4b80260b903f72e2c4416511bc9d35c4
salomovasliddin3004 <85295223+salomovasliddin3004@users.noreply.github.com>
2021-09-16 14:41:46 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,91 @@
 <?php 
+
 /**
- *Author: Tilon
- *
- *Telegram : @TILON
- */
-$API_KEY = '1756006247:AAGT_cktvsg84OSLJrdReaNr0PNSl2dmiE8';
-##------------------------------##
-define('API_KEY',$API_KEY);
-function bot($method,$datas=[]){
-    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
-    $res = curl_exec($ch);
-    if(curl_error($ch)){
-        var_dump(curl_error($ch));
-    }else{
-        return json_decode($res);
-    }
-}
- 
- function sendaction($chat_id, $action){
- bot('sendchataction',[
- 'chat_id'=>$chat_id,
- 'action'=>$action
- ]);
+* @NETPHP ni manba sifatida qo'llab-quvvatlang.
+*
+*/
+
+ini_set('memory_limit', '1024M'); 
+define('API_KEY','1756006247:AAGT_cktvsg84OSLJrdReaNr0PNSl2dmiE8'); 
+$telegram = json_decode(file_get_contents('php://input'),true);
+$user_id =
+$telegram['message']['chat']['id'];
+$url = $telegram['message']['text'];
+
+
+ if($url == "/start"){
+  bot(
+  'sendMessage', [
+   'chat_id'=> $user_id,
+   'text'=> 'سلام خوش آمدید . لطفا لینک فایل مورد نظر را ارسال کنید .',
+  ]);  
+ }else{
+   
+  if(filter_var($url, FILTER_VALIDATE_URL)){
+   bot('sendMessage', ['chat_id'=> $user_id,'text'=> 'در حال آپلود فایل ...']);
+   if(remote_file_size($url) < 50){
+    $fileName = upload($url); 
+    send_file( $user_id , $fileName);
+    bot('sendMessage', ['chat_id'=> $user_id,'text'=> 'https://YOUR_SITE.com/YourFolder/files/'.$fileName ]);
+   } //آدرس پوشه ای که روی هاستتون ساختید
+    
+   
+  }
+    
  }
- //====================ᵗᶦᵏᵃᵖᵖ======================//
-$update = json_decode(file_get_contents('php://input'));
-$message = $update->message;
-$from_id = $message->from->id;
-$chat_id = $message->chat->id;
-$text = $message->text;
-//====================ᵗᶦᵏᵃᵖᵖ======================//
-if(preg_match('/^\/([Ss]tart)/',$text)){
-$start_time = round(microtime(true) * 1000);
-      $send=  bot('sendmessage', [
-                'chat_id' => $chat_id,
-                'text' =>"Tezlik:",
-            ])->result->message_id;
-        
-                    $end_time = round(microtime(true) * 1000);
-                    $time_taken = $end_time - $start_time;
-                    bot('editMessagetext',[
-                        "chat_id" => $chat_id,
-                        "message_id" => $send,
-                        "text" => "Tezlik:" . $time_taken . "ms",
-                    ]);
-}
+
+
+
+ function bot($method,$datas=[]){
+   $url = "https://api.telegram.org/bot".API_KEY."/".$method; $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url); 
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); 
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas); 
+    $res = curl_exec($ch); 
+    if(curl_error($ch)){
+   var_dump(curl_error($ch)); 
+    }else{ 
+   return json_decode($res); 
+    } 
+ }
+ 
+ 
+ function remote_file_size($url){
+   $ch = curl_init($url);
+
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+   curl_setopt($ch, CURLOPT_HEADER, TRUE);
+   curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+
+   $data = curl_exec($ch);
+   $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+   curl_close($ch);
+   return round(($size/1024)/1024);
+ }
+
+ function upload($url){
+   $filename= preg_replace('/\\?.*/', '', basename($url));
+   $to = "files/".$filename;
+   $data=file_get_contents($url);
+   if($data===false) 
+   return false;
+   else{ 
+   file_put_contents($to,$data);
+   return $filename;
+  }  
+ }
+//<Botsorce>//
+ function send_file( $user_id , $fileName){
+   
+  $url= "https://api.telegram.org/bot".API_KEY."/sendDocument?chat_id=$user_id";
+  $post = array(
+   "document"  => new CURLFile(realpath('files/'.$fileName))
+  );
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+  curl_exec($ch);
+ } 
+
 ?>
